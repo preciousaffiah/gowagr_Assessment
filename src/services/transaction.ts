297ade96394bf0699673c5490e59175114bc167a
preciousaffiah <preciousaffiah205@gmail.com>
2024-10-22 13:54:07 +0100

    postman doc and transaction

--- src/services/transaction.ts
+++ src/services/transaction.ts
@@ -7,76 +7,80 @@ import {
 import { WalletService } from "./wallet";
 import { transactionData } from "@types*";
 import { TransactionRepository } from "@repositories";
-import { HttpException, generateREF } from "@utils";
+import { HttpException, generateREF, prisma } from "@utils";
 
 export class TransactionService {
   static async transfer(transactionData: transactionData) {
-    try {
-      const sender = await UserService.getUserById(transactionData.senderId);
-      const receiver = await UserService.getUserById(
-        transactionData.recipientId
-      );
-
-      if (transactionData.recipientId === transactionData.senderId) {
-        throw new HttpException(400, "You cannot send money to youself");
-      }
-
-      if (!sender.wallet || !receiver.wallet) {
-        throw new HttpException(400, "User does not have a wallet");
-      }
-
-      if (sender.wallet.balance < transactionData.amount) {
-        throw new HttpException(400, "Insufficient funds");
-      }
-
-      const initiatedTransactionRef = `TRF${generateREF()}`;
-
-      const senderTransactionData = {
-        amount: transactionData.amount,
-        type: TransactionTypeEnum.DEBIT,
-        description: transactionData.description,
-        recipientWalletId: receiver.wallet.walletId,
-        senderWalletId: sender.wallet.walletId,
-        refrence: initiatedTransactionRef,
-      };
-      const senderTransaction = await TransactionRepository.createTransaction(
-        senderTransactionData
-      );
-
-      const decrementSender = await WalletService.updateWalletBalance(
-        sender.wallet.walletId,
-        transactionData.amount,
-        WalletOperationEnum.DECREMENT
-      );
-      const incrementRecipient = await WalletService.updateWalletBalance(
-        receiver.wallet?.walletId,
-        transactionData.amount,
-        WalletOperationEnum.INCREMENT
-      );
-
-      const updateSenderTransaction =
-        await TransactionService.updateTransactionStatus(
-          senderTransaction?.transactionId as string,
-          TransactionStatusEnum.SUCCESSFULL
+    return await prisma.$transaction(async (prisma) => {
+      try {
+        const sender = await UserService.getUserById(transactionData.senderId);
+        const receiver = await UserService.getUserById(
+          transactionData.recipientId
         );
 
-      const recipientTransactionData = {
-        amount: transactionData.amount,
-        type: TransactionTypeEnum.CREDIT,
-        description: transactionData.description,
-        status: TransactionStatusEnum.SUCCESSFULL,
-        recipientWalletId: receiver.wallet.walletId,
-        senderWalletId: sender.wallet.walletId,
-        refrence: `TRF${generateREF()}`,
-      };
+        if (transactionData.recipientId === transactionData.senderId) {
+          throw new HttpException(400, "You cannot send money to youself");
+        }
 
-      const recipientTransaction =
-        await TransactionRepository.createTransaction(recipientTransactionData);
+        if (!sender.wallet || !receiver.wallet) {
+          throw new HttpException(400, "User does not have a wallet");
+        }
 
-      return updateSenderTransaction;
-    } catch (error: any) {
-      throw new HttpException(error.status, error.message);
-    }
+        if (sender.wallet.balance < transactionData.amount) {
+          throw new HttpException(400, "Insufficient funds");
+        }
+
+        const initiatedTransactionRef = `TRF${generateREF()}`;
+
+        const senderTransactionData = {
+          amount: transactionData.amount,
+          type: TransactionTypeEnum.DEBIT,
+          description: transactionData.description,
+          recipientWalletId: receiver.wallet.walletId,
+          senderWalletId: sender.wallet.walletId,
+          refrence: initiatedTransactionRef,
+        };
+        const senderTransaction = await TransactionRepository.createTransaction(
+          senderTransactionData
+        );
+
+        const decrementSender = await WalletService.updateWalletBalance(
+          sender.wallet.walletId,
+          transactionData.amount,
+          WalletOperationEnum.DECREMENT
+        );
+        const incrementRecipient = await WalletService.updateWalletBalance(
+          receiver.wallet?.walletId,
+          transactionData.amount,
+          WalletOperationEnum.INCREMENT
+        );
+
+        const updateSenderTransaction =
+          await TransactionService.updateTransactionStatus(
+            senderTransaction?.transactionId as string,
+            TransactionStatusEnum.SUCCESSFULL
+          );
+
+        const recipientTransactionData = {
+          amount: transactionData.amount,
+          type: TransactionTypeEnum.CREDIT,
+          description: transactionData.description,
+          status: TransactionStatusEnum.SUCCESSFULL,
+          recipientWalletId: receiver.wallet.walletId,
+          senderWalletId: sender.wallet.walletId,
+          refrence: `TRF${generateREF()}`,
+        };
+
+        const recipientTransaction =
+          await TransactionRepository.createTransaction(
+            recipientTransactionData
+          );
+
+        return updateSenderTransaction;
+      } catch (error: any) {
+        throw new HttpException(error.status, error.message);
+      }
+    });
   }
 
   static async updateTransactionStatus(
